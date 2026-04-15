5a6a84e89f2a62a09bdb4fbcbf6407afaa23af5e
wd2nf8gqct <committer@noreply.ethi.cz>
2026-04-14 22:15:24 -0400

    fix(base): remove headless vim invocation from activation

--- home/modules/base.nix
+++ home/modules/base.nix
@@ -148,7 +148,6 @@ in
       $DRY_RUN_CMD ${pkgs.curl}/bin/curl -fLo "$PLUG_VIM" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     fi
-    $DRY_RUN_CMD ${pkgs.vim}/bin/vim +'PlugInstall --sync' +qa || true
   '';
 
   home.activation.tmuxPlugins = lib.hm.dag.entryAfter ["writeBoundary"] ''
