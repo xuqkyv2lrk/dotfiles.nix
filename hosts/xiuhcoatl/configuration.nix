e9acf431d6adba45f4300f8bb74296d6cdc06d68
wd2nf8gqct <committer@noreply.ethi.cz>
2026-04-14 22:09:47 -0400

    chore(xiuhcoatl): limit boot entries to 5

--- hosts/xiuhcoatl/configuration.nix
+++ hosts/xiuhcoatl/configuration.nix
@@ -7,6 +7,7 @@
 
   # Boot
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
