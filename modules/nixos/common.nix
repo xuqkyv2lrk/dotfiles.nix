723156c67ea156093ae33e1ad74b227e42206bf5
wd2nf8gqct <committer@noreply.ethi.cz>
2026-05-19 17:05:13 -0400

    feat(common): enable Avahi and open Iriun ports

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -9,4 +9,13 @@
   boot.extraModprobeConfig = ''
     options v4l2loopback exclusive_caps=1 video_nr=2 card_label="Iriun Webcam"
   '';
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  networking.firewall.allowedTCPPorts = [ 5000 ];
+  networking.firewall.allowedUDPPorts = [ 5000 ];
 }
